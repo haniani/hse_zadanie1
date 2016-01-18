566081e56197f4a61c8a48a6ff4280bb074b6033
haniani <im@haniani.ru>
2016-01-18 03:55:12 +0300

    initial commit

---
+++ amhar.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# для работы необходим исходный текст + файл алфавита
+import os, fileinput
+
+file = open('al.txt') # таблица значений
+M = []
+dic = {}
+syllable = ''
+letter = ''
+
+for line in file: ### обходим файл
+    line = line.strip('\n') # убрать переносы
+    line = line.split('\t') # разбиваем на массивы
+    ar.append(line)
+
+for i in range(len(ar)): # обходим массив (i - строки)
+    #print(ar[i])
+    for x in range(len(ar[i])): # x - номер элемента в строке
+        if i != 0 and x != 0: # исключаем обработку ключа
+            j = ar[i][x]
+            m = ar[0][x] + ar[i][0] # m - слог, состоящий из номера строки и номера элемента
+            dr[j] = m # генерируем словарь
+
+def transliterate(string):
+    for i, j in dr.items():
+        string = string.replace(i,j)
+    return string
+
+
+
+with fileinput.FileInput('text.txt', inplace=True, backup='.bak') as file:
+    for line in file:
+        print(transliterate(line), end='')
